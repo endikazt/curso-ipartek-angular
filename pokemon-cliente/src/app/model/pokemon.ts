759e74f96750c9c1a40c83f692ead61d58babd99
Endika Zuazo <endikazt@hotmail.com>
2020-02-20 13:56:16 +0100

    Formulario con habilidades completado

--- pokemon-cliente/src/app/model/pokemon.ts
+++ pokemon-cliente/src/app/model/pokemon.ts
@@ -5,14 +5,14 @@ export class Pokemon {
     id: number;
     nombre: string;
     imagen: string;
-    habilidad: Array<Habilidad>;
+    habilidades: Array<Habilidad>;
 
     constructor(){
 
         this.id = 0;
         this.nombre = "";
         this.imagen = "https://i.etsystatic.com/12696278/r/il/bb21a8/1868980486/il_570xN.1868980486_d6zs.jpg";
-        this.habilidad = new Array<Habilidad>();
+        this.habilidades = new Array<Habilidad>();
 
     }
 

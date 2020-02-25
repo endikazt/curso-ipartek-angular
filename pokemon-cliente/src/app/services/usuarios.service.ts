01c31c238fb82b5290584e23de464036f333c803
Endika Zuazo <endikazt@hotmail.com>
2020-02-25 09:48:39 +0100

    Spec.ts eliminados

--- pokemon-cliente/src/app/services/usuarios.service.ts
+++ pokemon-cliente/src/app/services/usuarios.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IUsuarioService } from './IUsuario.service';
 import { Usuario } from '../model/usuario';
+import { HttpClient } from '@angular/common/http';
+import { exists } from 'fs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsuariosService implements IUsuarioService{
+
+export class UsuariosService{
 
   private storage : any;
   private usuario : Usuario;
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
 
     this.usuario = undefined;
     this.storage = window.sessionStorage;
@@ -26,31 +30,20 @@ export class UsuariosService implements IUsuarioService{
     }
   }
   
-  login(nombre: string, password: string): Usuario {
+  login(nombre: string, password: string) : Observable<any> {
 
-    const NOMBRE = 'admin';
-    const PASSWD = 'admin';
+    let usuario = new Usuario();
+    usuario.nombre = nombre;
+    usuario.password = password;
+    const URL_API = "http://localhost:8080/pokemon-rest/usuario/login";
+    console.trace('UsuarioServices login ' + nombre);
 
-    let usuarioBuscar : Usuario;
+    return this.http.post<any>(URL_API, usuario);
 
-    if(NOMBRE === nombre && PASSWD === password) {
-
-      usuarioBuscar = new Usuario();
-      usuarioBuscar.nombre = nombre;
-      usuarioBuscar.password = password;
-      usuarioBuscar.id = 99;
-
-     this.storage.setItem('usuarioStorage', JSON.stringify(usuarioBuscar));
-
-    } else {
-
-      this.storage.removeItem('usuarioStorage');
-
-    }
-
-    return usuarioBuscar;
   }
+
   cerrarSesion() {
    this.storage.removeItem('usuarioStorage');
   }
+
 }

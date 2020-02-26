b9de1461238afcdba4c2c459e2c9a9ace1bbbd47
Endika Zuazo <endikazt@hotmail.com>
2020-02-26 12:52:42 +0100

    Cambiada la URL para produccion

--- pokemon-cliente/src/global.ts
+++ pokemon-cliente/src/global.ts
@@ -4,4 +4,5 @@ export const GLOBAL = {
     'version' : '0.1'
 };
 
-export const API_URL = 'http://localhost:8080/pokemon-rest/api/';
+export const API_URL = 'http://192.168.0.50:8080/pokemon-rest-endika/api/';
+//export const API_URL = 'http://localhost:8080/pokemon-rest-endika/api/';
